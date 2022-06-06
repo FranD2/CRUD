bbd9a18f5f06b2e4086101faa6100edfebafa96a
leox64 <55474957+leox64@users.noreply.github.com>
2022-06-06 03:39:37 -0500

    referencias de index.php a user.php

--- create.php
+++ create.php
@@ -12,7 +12,7 @@
         if (mysqli_query($conn,$insert)){
             $_SESSION['message'] = 'Registro guardado exitosamente';
             $_SESSION['message_type'] = 'success'; #Función de bootstrap
-            header('Location:index.php');
+            header('Location:user.php');
         }else{
         echo "El registro no se pudo guardar". mysqli_error($conn);
         }         #Devuelve una cadena que describe el último error
